41d36ba3c3a5ca7fe7cb66ad152b4cc4b4515f73
Joe Zhou <zhoushuqun@gmail.com>
2007-11-18 13:54:02 +0000

    Add lang php for openid_setting.

--- plugins/openid/openid_setting.inc.php
+++ plugins/openid/openid_setting.inc.php
@@ -1,12 +1,14 @@
 <?php
+include_once language('openid_setting');
+
 if (!$discuz_uid) {
-	showmessage('请您登陆后访问本站，现在将转入登录页面。', 'logging.php?action=login');
+	showmessage($GLOBALS['language']['openid_login_first'], 'logging.php?action=login');
 }
 
 require_once DISCUZ_ROOT . './plugins/openid/openid.func.php';
 require_once DISCUZ_ROOT . './plugins/openid/class.openid.php';
 
-$this_url = 'plugin.php?identifier=openid4discuz&module=openidsetting';
+$this_url = 'plugin.php?identifier=openid4discuz&module=openid_setting';
 
 if ($_POST['formhash'] != '' && $_POST['openid_url'] == '') {
 	// Remove the binding between member account and openid
@@ -14,18 +16,25 @@ if ($_POST['formhash'] != '' && $_POST['openid_url'] == '') {
 	$query = $db->query("DELETE FROM {$tablepre}openid WHERE uid = " . $discuz_uid);
 	$rows = $db->affected_rows($query);
 	if ($rows) {
-		showmessage('已删除你的账号和 OpenID 之间的绑定。', $this_url);
+		showmessage($GLOBALS['language']['openid_delete_success'], $this_url);
 	} else {
-		showmessage('你的账号尚未绑定任何 OpenID。', $this_url);
+		showmessage($GLOBALS['language']['openid_delete_failed'], $this_url);
 	}
 }
 elseif ($_POST['formhash'] != '' && $_POST['openid_url'] != '') {
 	// echo $_POST['openid_url'];
-	$query = $db->query("SELECT openid_url FROM {$tablepre}openid WHERE openid_url='" . $_POST['openid_url'] . "'");
+	$query = $db->query("SELECT openid_url FROM {$tablepre}openid WHERE uid<>" . $discuz_uid . " AND openid_url='" . $_POST['openid_url'] . "'");
 	$member_openid = $db->fetch_array($query);
 	if ($member_openid['openid_url']) {
-		showmessage("你输入的 OpenID 已与另一个账号绑定。", $this_url);
+		showmessage($GLOBALS['language']['openid_bind_failed'], $this_url);
 	}
+
+	$query = $db->query("SELECT openid_url FROM {$tablepre}openid WHERE uid=" . $discuz_uid);
+	$old_openid = $db->fetch_array($query);
+	if ($old_openid['openid_url'] == $_POST['openid_url']) {
+		showmessage($GLOBALS['language']['openid_update_noupdate_before'] . "<a href='" . $old_openid['openid_url'] . "'>" . $old_openid['openid_url'] . "</a>" . $GLOBALS['language']['openid_update_noupdate_after'], $this_url);
+	}
+
 	// Redirect user to OpenID Server
 	$openid = new SimpleOpenID;
 	$openid->SetIdentity($openid_url);
@@ -45,23 +54,23 @@ elseif ($_GET['openid_mode'] == 'id_res') {
 	$openid->SetIdentity($_GET['openid_identity']);
 	$openid_validation_result = $openid->ValidateWithServer();
 	if ($openid_validation_result == true) { // OK HERE KEY IS VALID
-		$query = $db->query("SELECT openid_url FROM {$tablepre}openid WHERE openid_url='" . $openid->GetIdentity() . "'");
+		$query = $db->query("SELECT openid_url FROM {$tablepre}openid WHERE uid <> " . $discuz_uid . " AND  openid_url='" . $openid->GetIdentity() . "'");
 		$member_openid = $db->fetch_array($query);
 		if ($member_openid['openid_url']) {
-			showmessage("你输入的 OpenID 已与另一个账号绑定。", $this_url);
+			showmessage($GLOBALS['language']['openid_bind_failed'], $this_url);
 		}
 
 		$query = $db->query("SELECT openid_url FROM {$tablepre}openid WHERE uid=" . $discuz_uid);
 		$old_openid = $db->fetch_array($query);
 		if (!$old_openid['openid_url']) {
 			$db->query("INSERT {$tablepre}openid(uid, openid_url) VALUES(" . $discuz_uid . ", '" . $openid->GetIdentity() . "')");
-			showmessage('你的账号和 OpenID（<a href="' . $openid->GetIdentity() . '">' . $openid->GetIdentity() . '</a>）已建立绑定，你现在可以使用 OpenID 来登录论坛了。', $this_url);
+			showmessage($GLOBALS['language']['openid_insert_success_before'] . '<a href="' . $openid->GetIdentity() . '">' . $openid->GetIdentity() . '</a>' . $GLOBALS['language']['openid_insert_success_after'], $this_url);
 		}
 		elseif ($old_openid['openid_url'] != $openid->GetIdentity()) {
 			$query = $db->query("UPDATE {$tablepre}openid SET openid_url = '" . $openid->GetIdentity() . "' WHERE uid = " . $discuz_uid);
-			showmessage('你的账号和 OpenID（<a href="' . $openid->GetIdentity() . '">' . $openid->GetIdentity() . '</a>）之间的关联已更新，你现在可以使用新的 OpenID 来登录论坛了。', $this_url);
+			showmessage($GLOBALS['language']['openid_update_success_before'] . '<a href="' . $openid->GetIdentity() . '">' . $openid->GetIdentity() . '</a>' . $GLOBALS['language']['openid_update_success_after'], $this_url);
 		} else {
-			showmessage("你没有修改 OpenID，请继续使用“<a href='" . $old_openid['openid_url'] . "'>" . $old_openid['openid_url'] . "</a>”登录。", $this_url);
+			showmessage($GLOBALS['language']['openid_update_noupdate_before'] . "<a href='" . $old_openid['openid_url'] . "'>" . $old_openid['openid_url'] . "</a>" . $GLOBALS['language']['openid_update_noupdate_after'], $this_url);
 		}
 	}
 	elseif ($openid->IsError() == true) { // ON THE WAY, WE GOT SOME ERROR
@@ -79,5 +88,5 @@ elseif ($_GET['openid_mode'] == 'cancel') { // User Canceled your Request
 $query = $db->query("SELECT openid_url FROM {$tablepre}openid WHERE uid = " . $discuz_uid);
 $openid = $db->fetch_array($query);
 
-include template('openid-setting');
+include template('openid_setting');
 ?>
