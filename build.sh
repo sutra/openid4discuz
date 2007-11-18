89e3d2b60a527852fc5a8ff1bef6cdb774b4f91a
Joe Zhou <zhoushuqun@gmail.com>
2007-11-18 15:08:33 +0000

    Add gbk tar to build.sh.

--- build.sh
+++ build.sh
@@ -18,8 +18,12 @@ tar jcfv build/openid4discuz-UTF-8-$VERSION-$date.tar.bz2 \
 	templates/default/openid_setting.htm \
 	templates/default/openid_setting.lang.php \
 	templates/default/login.htm
-cd build
-tar xvf openid4discuz-UTF-8-$VERSION-$date.tar.bz2 openid4discuz-GBK-$VERSION-$date
-#cp -R build/openid4discuz-UTF-8-$VERSION/* build/openid4discuz-GBK-$VERSION/
-#iconv -f UTF-8 -t GBK build/openid4discuz-GBK-$VERSION/templates/default/*.lang.php
-#tar jcfv build/openid4discuz-GBK-1.0.0-$date.tar.bz2 build/openid4discuz-GBK-$VERSION/*
+mkdir build/openid4discuz-GBK-$VERSION-$date
+tar xvf build/openid4discuz-UTF-8-$VERSION-$date.tar.bz2 -C build/openid4discuz-GBK-$VERSION-$date
+iconv -f UTF-8 -t GBK build/openid4discuz-GBK-$VERSION-$date/templates/default/openid.lang.php > build/openid4discuz-GBK-$VERSION-$date/templates/default/openid.lang.php.tmp
+mv build/openid4discuz-GBK-$VERSION-$date/templates/default/openid.lang.php.tmp build/openid4discuz-GBK-$VERSION-$date/templates/default/openid.lang.php
+iconv -f UTF-8 -t GBK build/openid4discuz-GBK-$VERSION-$date/templates/default/openid_install.lang.php > build/openid4discuz-GBK-$VERSION-$date/templates/default/openid_install.lang.php.tmp
+mv build/openid4discuz-GBK-$VERSION-$date/templates/default/openid_install.lang.php.tmp build/openid4discuz-GBK-$VERSION-$date/templates/default/openid_install.lang.php
+iconv -f UTF-8 -t GBK build/openid4discuz-GBK-$VERSION-$date/templates/default/openid_setting.lang.php > build/openid4discuz-GBK-$VERSION-$date/templates/default/openid_setting.lang.php.tmp
+build/openid4discuz-GBK-$VERSION-$date/templates/default/openid_setting.lang.php.tmp build/openid4discuz-GBK-$VERSION-$date/templates/default/openid_setting.lang.php
+tar jcfv build/openid4discuz-GBK-1.0.0-$date.tar.bz2 build/openid4discuz-GBK-$VERSION-$date/*
