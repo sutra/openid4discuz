cee4b302617650e651d46f1b1a854119dfbc3e8e
Joe Zhou <zhoushuqun@gmail.com>
2007-11-20 15:13:51 +0000

    Fix installing sql.

--- openid_install.php
+++ openid_install.php
@@ -6,14 +6,19 @@ require_once './include/common.inc.php';
 
 include_once language('openid_install');
 
+if ($adminid != 1) {
+	showmessage("Your are not the administrator of this forum.");
+}
+
 if ($_POST["installsubmit"]) {
-	$sql = "CREATE TABLE IF NOT EXISTS `{$tablepre}openid` (" 
-		. "`uid` mediumint(8) unsigned NOT NULL," 
-		. "`openid_url` char(255) NOT NULL," 
-		. "PRIMARY KEY (`openid_url`)," 
-		. "UNIQUE KEY `uid` (`uid`)" 
-		. ") ENGINE=MyISAM DEFAULT CHARSET=utf8;";
+	$sql = "CREATE TABLE IF NOT EXISTS `{$tablepre}openid` ("
+		. "`uid` mediumint(8) unsigned NOT NULL,"
+		. "`openid_url` varchar(255) NOT NULL,"
+		. "PRIMARY KEY (`openid_url`),"
+		. "UNIQUE KEY `uid` (`uid`)"
+		. ");";
 	$db->query($sql);
+	showmessage("Install OK.");
 }
 
 include template('openid_install');
