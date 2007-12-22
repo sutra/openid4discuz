17d674aaad69e2b1eeda6e6a1211d6d2335fa2e6
Joe Zhou <zhoushuqun@gmail.com>
2007-12-22 20:13:35 +0000

    Modify version number.

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ PROJECT=openid4discuz
 baseDirForScriptSelf=$(cd "$(dirname "$0")"; pwd)
 BUILD=${baseDirForScriptSelf}/build
 DATE=`date +%Y-%m-%d`
-VERSION="1.0.0"-$DATE
+VERSION="2.0.0-SNAPSHOT"-$DATE
 
 PKG_UTF8=$BUILD/$PROJECT-$VERSION-UTF-8.tar.gz
 PKG_GBK=$BUILD/$PROJECT-$VERSION-GBK.tar.gz
