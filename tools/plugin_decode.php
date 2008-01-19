8d4cc7de4147de4e0743af22bee59a9bf3ea0be0
Joe Zhou <zhoushuqun@gmail.com>
2008-01-19 12:03:56 +0000

    Add tools: plugin_decode & plugin_encode.

---
+++ tools/plugin_decode.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * Created on 2008-1-19
+ *
+ * 把Discuz!插件导入代码转换为人可读的源码。
+ * 
+ * <p>感谢<a href="http://discuz.net">Discuz! 官方论坛</a>的网友 C43F，介绍我了解了<a 
+ * href="http://www.discuz.net/thread-414617-1-1.html">BASE64内容编辑器</a></p>
+ * 
+ * @author sutra
+ * @license http://openid4discuz.redv.com/LICENSE.txt New BSD License
+ * @see <a href="http://www.discuz.net/thread-414617-1-1.html">BASE64内容编辑器</a>
+ */
+define('IN_DISCUZ', '1');
+require_once '../include/cache.func.php'; // for function `arrayeval'
+$data = file_get_contents("php://stdin", "r");
+$data = preg_replace("/(#.*\s+)*/", '', $data);
+$data = base64_decode($data);
+$array = unserialize($data);
+if (isset($array['plugin']['modules'])) $array['plugin']['modules'] = unserialize($array['plugin']['modules']);
+$data = arrayeval($array);
+echo $data;
+?>
