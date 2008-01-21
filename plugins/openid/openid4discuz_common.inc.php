be80e4fe830c19170b35bb43aa02dcb240ab6291
Joe Zhou <zhoushuqun@gmail.com>
2008-01-21 17:12:43 +0000

    Move openid functions to OpenID4DiscuzUtils.

--- plugins/openid/openid4discuz_common.inc.php
+++ plugins/openid/openid4discuz_common.inc.php
@@ -11,8 +11,14 @@
  */
 define('OPENID4DISCUZ_ROOT', dirname(__FILE__));
 define('OPENID4DISCUZ_DISCUZ_ROOT', dirname(dirname(dirname(__FILE__))));
-require_once OPENID4DISCUZ_DISCUZ_ROOT.'/plugins/openid/openid.func.php';
+
+// OpenID4Discuz utilities.
+require_once OPENID4DISCUZ_ROOT.'/util/OpenID4DiscuzUtils.php';
+
+// OpenID4Discuz manager.
 require_once OPENID4DISCUZ_ROOT."/OpenID4Discuz.php";
+
+// for php-openid.
 require_once OPENID4DISCUZ_ROOT."/common.php";
 
 $openid4discuz = new OpenID4Discuz($tablepre, $db);
