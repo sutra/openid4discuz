6595abc17d23b20409442eedab953e9bb4c15abc
Joe Zhou <zhoushuqun@gmail.com>
2008-01-29 14:40:26 +0000

    Rename included files.

--- openid.php
+++ openid.php
@@ -5,19 +5,19 @@
  * @license http://openid4discuz.redv.com/LICENSE.txt BSD
  */
 require_once 'include/common.inc.php';
-require_once DISCUZ_ROOT.'./plugins/openid/openid.func.php';
+require_once DISCUZ_ROOT.'./plugins/openid/openid4discuz_common.inc.php';
 require_once DISCUZ_ROOT.'./forumdata/cache/plugin_openid4discuz.php';
 
 $action = $_GET['action'];
 if ($action == 'finish_auth') {
-	include 'plugins/openid/finish_auth.php';
+	include 'plugins/openid/finish_auth.inc.php';
 } elseif ($action == 'finish_auth_openid_setting') {
-	include 'plugins/openid/finish_openid_setting.php';
+	include 'plugins/openid/finish_openid_setting.inc.php';
 } else {
 	if (empty($openid_identifier)) {
 		showmessage('请输入OpenID。', dreferer());
 	} else {
-		include 'plugins/openid/try_auth.php';
+		include 'plugins/openid/try_auth.inc.php';
 	}
 }
 ?>
