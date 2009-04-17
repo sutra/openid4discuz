dceec07163a2ecda64757b35824e1b397b35117a
Joe Zhou <zhoushuqun@gmail.com>
2009-04-17 19:32:35 +0000

    Add flowchart of openid4ucenter.

---
+++ doc/openid4ucenter.mm
@@ -0,0 +1,15 @@
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1239815898243" ID="Freemind_Link_489739770" MODIFIED="1239815906187" TEXT="OpenID4UCenter">
+<node CREATED="1239815916519" ID="_" MODIFIED="1239815926124" POSITION="right" TEXT="&#x767b;&#x5f55;">
+<node CREATED="1239816089649" ID="Freemind_Link_542748791" MODIFIED="1239816490768" TEXT="&#x6839;&#x636e;OpenID&#x83b7;&#x5f97;&#x8be5;OpenID&#x7ed1;&#x5b9a;&#x7684;&#x7528;&#x6237;&#x5217;&#x8868;&#xa;&#xff08;getUCenterUserIDsByOpenID&#xff0c;uc_get_user&#xff09;">
+<node CREATED="1239815975998" ID="Freemind_Link_722938045" MODIFIED="1239816065002" TEXT="&#x540c;&#x6b65;&#x767b;&#x5f55;&#xff08;uc_user_synlogin&#xff09;"/>
+<node CREATED="1239816243315" ID="Freemind_Link_1830797738" MODIFIED="1239816276274" TEXT="&#x9009;&#x62e9;&#x6ce8;&#x518c;&#x65b0;&#x8d26;&#x53f7;&#x6216;&#x8005;&#x7ed1;&#x5b9a;&#x5df2;&#x6709;&#x8d26;&#x53f7;">
+<node CREATED="1239816280622" ID="Freemind_Link_1302321121" MODIFIED="1239816308738" TEXT="&#x6ce8;&#x518c;&#x65b0;&#x8d26;&#x53f7;&#xff08;uc_user_register&#xff09;"/>
+<node CREATED="1239816327177" ID="Freemind_Link_688729460" MODIFIED="1239816337626" TEXT="&#x767b;&#x5f55;&#x5df2;&#x6709;&#x8d26;&#x53f7;&#xff08;uc_user_login&#xff09;"/>
+</node>
+</node>
+</node>
+<node CREATED="1239815932441" ID="Freemind_Link_685873907" MODIFIED="1239815951019" POSITION="right" TEXT="&#x7528;&#x6237;&#x7684;OpenID&#x8bbe;&#x7f6e;"/>
+</node>
+</map>
